593f93f27d024e0a18183fd67b0bf1c86e3091f9
Ansh Tiwari <ansh.tiwari2025@glbajajgroup.org>
2026-06-14 11:34:23 +0530

    Add DAY-6 assignments

---
+++ DAY-6/d.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int x, n, result = 1;
+
+    printf("Enter base and power: ");
+    scanf("%d%d", &x, &n);
+
+    for (int i = 1; i <= n; i++) {
+        result = result * x;
+    }
+
+    printf("%d^%d = %d", x, n, result);
+
+    return 0;
+}
